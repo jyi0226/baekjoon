a44007f857e7c5dfbb50e64cae33747884414b8d
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2023-12-08 23:48:32 +0900

    Gold 3 : 피리 부는 사나이

---
+++ Gold/16724.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int p[1010101];
+bool vis[1010101]={};
+int find_p(int x){
+    if(x==p[x])return p[x];
+    return p[x]=find_p(p[x]);
+}
+
+void union_p(int x,int y){
+    x=find_p(x);
+    y=find_p(y);
+    
+    if(x<y)p[x]=y;
+    else p[y]=x;
+}
+
+char board[1010][1010];
+int num[1010][1010];
+int n,m;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>n>>m;
+    int cnt=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>board[i][j];
+            p[cnt]=cnt;
+            vis[cnt]=0;
+            num[i][j]=cnt++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(board[i][j]=='D'){
+                union_p(num[i][j],num[i+1][j]);
+            }
+            else if(board[i][j]=='U'){
+                union_p(num[i][j],num[i-1][j]);
+            }
+            else if(board[i][j]=='L'){
+                union_p(num[i][j],num[i][j-1]);
+            }
+            else if(board[i][j]=='R'){
+                union_p(num[i][j],num[i][j+1]);
+            }
+        }
+    }
+    cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(!vis[find_p(num[i][j])]){
+                vis[find_p(num[i][j])]=1;
+                cnt++;
+            }
+        }
+    }
+    cout<<cnt;
+}
