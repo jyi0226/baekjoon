a744b5422874b5a9580a5c27bb0cc98a2cb1df3a
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2023-12-11 22:27:12 +0900

    Gold 2 : 홀수 번째 피보나치 수의 합

---
+++ Gold/11442.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int mod=1000000007;
+map<long,long> m;
+long long n;
+
+long long fib(long long num){
+    if(m[num])return m[num];
+    long long res;
+    if(num%2==0)res=(fib(num/2)*(fib(num/2+1)+fib(num/2-1)))%mod;
+    else res=((fib((num-1)/2)*fib((num-1)/2))%mod+(fib((num+1)/2)*fib((num+1)/2))%mod)%mod;
+    
+    return m[num]=res%mod;
+}
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    m[0]=0;
+    m[1]=1;
+    m[2]=1;
+    cin>>n;
+    if(n%2==0){
+        cout<<(fib(n))%mod;
+    }
+    else{
+        cout<<(fib(n+1))%mod;    
+    }
+}
