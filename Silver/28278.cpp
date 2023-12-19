cfdbbc099ff59e2c3bae439b8c65c2281cd637b0
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2023-12-19 23:05:52 +0900

    Silver 4 : 스택2

---
+++ Silver/28278.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int n,ord,num;
+stack<int> s;
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>ord;
+        if(ord==1){
+            cin>>num;
+            s.push(num);
+        }
+        else if(ord==2){
+            if(!s.empty()){
+                cout<<s.top()<<"\n";
+                s.pop();
+            }
+            else cout<<"-1\n";
+        }
+        else if(ord==3){
+            cout<<s.size()<<"\n";
+        }
+        else if(ord==4){
+            cout<<s.empty()<<"\n";
+        }
+        else{
+            if(!s.empty()){
+                cout<<s.top()<<"\n";
+            }
+            else cout<<"-1\n";
+        }
+    }
+}
