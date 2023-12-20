60db3f54e95b3a296e6c878ada57c2a524aed0eb
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2023-12-20 22:26:53 +0900

    Silver 4 : 인사성 밝은 곰돌이

---
+++ Silver/25192.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+using namespace std;
+string s;
+map<string,int> m;
+int n,cnt=0;
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        if(s=="ENTER"){
+            m.clear();
+        }
+        else{
+            if(m[s])continue;
+            m[s]=1;
+            cnt++;
+        }
+    }
+    cout<<cnt;
+}
