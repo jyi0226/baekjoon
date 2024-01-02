9d99a2e937074985de2621570510a700e8b70d0d
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2024-01-02 15:44:23 +0900

    Create 1931.cpp

---
+++ Silver/1931.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int a,b;
+vector<pair<int,int>> v;
+int t;
+int cnt=1;
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        v.push_back({b,a});
+    }
+    sort(v.begin(),v.end());
+    t=v[0].first;
+    for(int i=1;i<n;i++){
+        if(t<=v[i].second){
+            cnt++;
+            t=v[i].first;
+        }
+    }
+    cout<<cnt;
+}
