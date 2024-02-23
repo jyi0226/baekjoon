c8d60b0acaed4b0090ac4a978f020df87b6c981e
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2024-02-23 13:01:54 +0900

    Update 중간저장용.cpp

--- 중간저장용.cpp
+++ 중간저장용.cpp
@@ -1,64 +1,23 @@
 #include <iostream>
-#include <queue>
-#include <vector>
+#include <stack>
 using namespace std;
-
-int n,m,p,num;
-int dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
-vector<pair<int,int>> P[10];
-char board[1010][1010];
-bool vis[1010][1010];
-int cnt[10];
-void func(){
-    queue<pair<int,int>> q,q2;
-    for(int i=1;i<=p;i++){
-        for(int j=0;j<P[i].size();j++){
-            q.push(P[i][j]);
-            vis[P[i][j].first][P[i][j].second]=1;
-            cnt[i]++;
-        }
-    }
-    while(!q.empty()){
-        q2.push(q.front());
-        q.pop();
-        while(!q.empty()){
-            if(board[q.front().first][q.front().second]!=board[q2.front().first][q2.front().second])break;
-            
-    while(!q.empty()){
-        int cx=q.front().first;
-        int cy=q.front().second;
-        q.pop();
-        char val=board[cx][cy];
-        for(int i=0;i<4;i++){
-            int nx=cx+dx[i];
-            int ny=cy+dy[i];
-            if(nx<0||ny<0||nx>=n||ny>=m)continue;
-            
-            if(!vis[nx][ny]&&board[nx][ny]=='.'){
-                board[nx][ny]=val;
-                cnt[val-'0']++;
-                vis[nx][ny]=1;
-                q.push({nx,ny});
-            }
-        }
-    }
-}
+int n;
+long long h[101010];
 int main()
 {
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin>>n>>m>>p;
-    for(int i=0;i<p;i++)cin>>num;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cin>>board[i][j];
-            if(board[i][j]!='.'&&board[i][j]!='#'){
-                P[board[i][j]-'0'].push_back({i,j});
+    ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    while(1){
+        cin>>n;
+        if(n==0)break;
+        stack<int> s;
+        for(int i=0;i<n;i++)cin>>h[i];
+        for(int i=0;i<n;i++){
+            if(s.empty())s.push(i);
+            else{
+                if(s.top())
             }
         }
+        
+        
     }
-    func();
-    
 }
