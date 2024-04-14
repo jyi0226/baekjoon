67815c90c8f43a63d8dcc4989239b3799126f7c8
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2024-04-14 14:06:08 +0900

    Gold 1 : 비숍

---
+++ 1799.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool board[11][11];
+bool up[40],down[40];
+int n,res=0;
+
+void func(int cur,int cnt){
+    if(cur>=2*n-1){
+        res=max(cnt,res);
+    }
+    else{
+        bool flag=0;
+        if(cur<=n){
+            for(int i=0;i<=cur;i++){
+                int x=cur-i;
+                int y=i;
+
+                if(board[x][y]&&!up[cur]&&!down[x-y+n]){
+                    flag=1;
+                    up[cur]=1;
+                    down[x-y+n]=1;
+                    func(cur+1,cnt+1);
+                    up[cur]=0;
+                    down[x-y+n]=0;
+                }
+            }
+        }
+        else{
+            for(int i=cur-n+1;i<n;i++){
+                int x=cur-i;
+                int y=i;
+
+                if(board[x][y]&&!up[cur]&&!down[x-y+n]){
+                    flag=1;
+                    up[cur]=1;
+                    down[x-y+n]=1;
+                    func(cur+1,cnt+1);
+                    up[cur]=0;
+                    down[x-y+n]=0;
+                }
+            }
+        }
+        if(!flag)func(cur+1,cnt);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(nullptr);
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)cin>>board[i][j];
+    }
+    func(0,0);
+    cout<<res;
+}
