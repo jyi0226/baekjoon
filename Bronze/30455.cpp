f029b1367ee4a96cc835f3a3456e6f4491a6a2f6
Youngin Jeong <105589421+jyi0226@users.noreply.github.com>
2024-12-01 17:53:32 +0900

    Bronze 1 : 이제는 더 이상 물러날 곳이 없다

---
+++ Bronze/30455.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int t,d,n,s,p;
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(nullptr);
+	cin>>n;
+	if(n%2!=0)cout<<"Goose";
+	else cout<<"Duck";
+}
