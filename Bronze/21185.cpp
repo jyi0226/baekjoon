c1f052cbdf14c4bed8395267fca416e0b8090fc6
jyi0226 <andy0372@naver.com>
2025-11-17 15:14:42 +0900

    Bronze 3 : Some Sum

---
+++ Bronze/21185.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(nullptr);
+
+    cin >> n;
+    if (n == 1)
+        cout << "Either";
+    else if (n == 2)
+        cout << "Odd";
+    else if (n == 10)
+        cout << "Odd";
+    else if (n % 2 == 0)
+        cout << "Even";
+    else
+        cout << "Either";
+}
